4f0a2ebe87ca93cdcda58bd41fdcaa3aad815821
Nozomi Ishii <non.ankt@gmail.com>
2021-01-05 11:19:07 +0900

    feat: create countdown components

---
+++ src/components/countDown.ts
@@ -0,0 +1,30 @@
+export interface Time {
+  total: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+/**
+ * endTime format should be like '2030/05/09'
+ * https://qiita.com/kodama321/items/834f5fb721b533b73fa9
+ */
+export const countDown = (endTime: string): Time => {
+  const now = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
+  const end = new Date(endTime).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
+
+  const total = (Date.parse(end) - Date.parse(now)) / 1000; // convert 1000ms to 1s
+  const days = Math.floor(total / (60 * 60 * 24));
+  const hours = Math.floor((total / (60 * 60)) % 24);
+  const minutes = Math.floor((total / 60) % 60);
+  const seconds = Math.floor(total % 60);
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import { countDown, Time } from 'src/components';
+import Head from 'next/head';
+
 const Home: React.FC = () => {
-  return <div className="bg-green-500">!!!!nozomi ishii 2021!!!</div>;
+  const endTime = '2021/01/11';
+  const [count, setCount] = useState<Time>();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(countDown(endTime));
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>the day</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="bg-green-500 min-h-screen flex justify-center items-center">
+        {count &&
+          `${count.days}days ${count.hours}hours ${count.minutes}minutes ${count.seconds}seconds`}
+      </div>
+    </>
+  );
 };
 
 export default Home;
