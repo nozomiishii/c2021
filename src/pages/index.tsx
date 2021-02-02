1ed910150023077ddad58ff011527f8a7ce84322
Nozomi Ishii <non.ankt@gmail.com>
2021-02-03 01:23:52 +0900

    feat: loading message for main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,11 @@ import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { NavbarBgBlack } from 'src/components/Navbar/NavbarBgBlack';
 
-const DanceCodeArt = dynamic(() => import('../components/DanceCodeArt'), { ssr: false });
+const Loading = () => <p>Loading ...</p>;
+const DanceCodeArt = dynamic(() => import('../components/DanceCodeArt'), {
+  loading: Loading,
+  ssr: false,
+});
 
 const Home: React.FC = () => (
   <>
