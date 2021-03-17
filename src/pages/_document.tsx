5e25323945a5834f2e935b686e61fdb74f6cfeae
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-03-18 01:40:29 +0900

    🐞: update meta data on head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,23 +10,29 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="application-name" content="nozomiishii" />
+          <meta name="theme-color" content="#fff" />
+          <meta
+            name="description"
+            content="Dance + Code + Art | Frontend Engineer who is passionate about dance and 3D arts"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+
+          {/* windows config */}
           <meta name="msapplication-square70x70logo" content="/site-tile-70x70.png" />
           <meta name="msapplication-square150x150logo" content="/site-tile-150x150.png" />
           <meta name="msapplication-wide310x150logo" content="/site-tile-310x150.png" />
           <meta name="msapplication-square310x310logo" content="/site-tile-310x310.png" />
           <meta name="msapplication-TileColor" content="#fff" />
 
+          {/* safari config */}
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="#fff" />
           <meta name="apple-mobile-web-app-title" content="myapp" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-
-          <meta name="application-name" content="nozomiishii" />
-          <meta name="theme-color" content="#fff" />
-          <meta name="description" content="nozomi" />
-          <link rel="icon" sizes="192x192" href="/icon-192x192.png" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body className="bg-gray-100">
           <Main />
