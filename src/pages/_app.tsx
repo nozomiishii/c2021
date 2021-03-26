3eb0d150cc95c603280d824756b163113101c4b6
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-03-26 17:08:54 +0900

    ♻️: move viewport to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import 'react-notion/src/styles.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import '../styles/tailwind.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="application-name" content="NOZOMI ISHII" />
           <meta name="theme-color" content="#fff" />
           <meta
