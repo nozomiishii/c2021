83895100266be2a8f4c4fb2510f3f5b5eea1f9e8
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-03-31 23:22:59 +0900

    ♻️: prettier-plugin-tailwind

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -17,25 +17,25 @@ export const Navbar: React.FC = () => {
 
   const Menu = navbarItems.map(({ name, slug }) => (
     <Link href={slug} key={name}>
-      <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter text-gray-800 hover:text-green-700 mr-4">
+      <a className="text-teal-lighter block mr-4 mt-4 text-gray-800 hover:text-green-700 lg:inline-block lg:mt-0">
         {name}
       </a>
     </Link>
   ));
 
   return (
-    <nav className="flex py-2 px-4 items-center justify-between flex-wrap lg:p-4 fixed w-full">
+    <nav className="fixed flex flex-wrap items-center justify-between px-4 py-2 w-full lg:p-4">
       {/* logo */}
-      <div className="text-gray-800 mr-6">
+      <div className="mr-6 text-gray-800">
         <Link href="/">
-          <a className="font-semibold text-xl tracking-tight">NOZOMI ISHII</a>
+          <a className="text-xl font-semibold tracking-tight">NOZOMI ISHII</a>
         </Link>
       </div>
 
       {/* toggle button for mobile */}
       <div className="block lg:hidden">
         <button
-          className="flex justify-center items-center w-8 h-8 border rounded-full border-gray-800 hover:text-green-700 hover:border-green-700"
+          className="flex items-center justify-center w-8 h-8 hover:text-green-700 border border-gray-800 hover:border-green-700 rounded-full"
           onClick={() => setOpen((state) => !state)}
         >
           🦸🏻‍♂️
@@ -48,7 +48,7 @@ export const Navbar: React.FC = () => {
       </div>
       {/* for mobile TODO: 上と共通化する*/}
       {open && (
-        <div className="w-full text-sm flex flex-col items-end lg:flex-row lg:items-center lg:w-auto">
+        <div className="flex flex-col items-end w-full text-sm lg:flex-row lg:items-center lg:w-auto">
           {Menu}
         </div>
       )}
