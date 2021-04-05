8bfb9931d84f2eef2ea9266bc27f871a97fa4958
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-04-05 20:46:25 +0900

    🚀: update svg module to svgr

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
   images: {
     domains: ['www.notion.so'],
   },
   pwa: {
+    mode: 'production',
     disable: process.env.NODE_ENV === 'development',
+    register: true,
     dest: 'public',
+    sw: 'service-worker.js',
   },
 });
