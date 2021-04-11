74dc6c514dc33db1200cc984c9ab77d351f75039
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-04-12 01:52:57 +0900

    ✨: add page transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,28 @@ import Head from 'next/head';
 import 'react-notion/src/styles.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import '../styles/tailwind.css';
+import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Component {...pageProps} />
+      <div className="min-h-screen bg-gray-800">
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={router.route}
+            initial={{ opacity: 0.5 }}
+            animate={{ opacity: 1 }}
+            exit={{ x: '-100vw', transition: { ease: 'easeInOut', duration: 0.3 } }}
+            className="bg-white"
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </div>
     </>
   );
 };
